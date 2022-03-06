a5a17dedf4d02b59bbd15bce8697151a7e974444
alexkmnsky <29873742+alexkmnsky@users.noreply.github.com>
2022-03-06 11:50:53 -0500

    Add const getters to Transform, mark methods with nodiscard

--- Source/Transform.h
+++ Source/Transform.h
@@ -6,38 +6,41 @@
 class Transform
 {
 public:
-	Transform(const glm::vec3& position = glm::vec3(),
-		const glm::vec3& rotation = glm::vec3(),
-		const glm::vec3& scale = glm::vec3(1.0f, 1.0f, 1.0f))
-	{
-		this->position = position;
-		this->rotation = rotation;
-		this->scale = scale;
-	}
+	Transform(const glm::vec3& position = glm::vec3(), const glm::vec3& rotation = glm::vec3(),
+		const glm::vec3& scale = glm::vec3(1.0f, 1.0f, 1.0f)) : position(position),
+		rotation(rotation), scale(scale) {}
 
-	inline glm::mat4 GetModel() const
+	[[nodiscard]] glm::mat4 GetModel() const
 	{
-		glm::mat4 positionMatrix = glm::translate(position);
-		glm::mat4 rotationXMatrix = glm::rotate(glm::radians(rotation.x), glm::vec3(1.0, 0.0, 0.0));
-		glm::mat4 rotationYMatrix = glm::rotate(glm::radians(rotation.y), glm::vec3(0.0, 1.0, 0.0));
-		glm::mat4 rotationZMatrix = glm::rotate(glm::radians(rotation.z), glm::vec3(0.0, 0.0, 1.0));
-		glm::mat4 scaleMatrix = glm::scale(scale);
+		const glm::mat4 positionMatrix = glm::translate(position);
 
-		glm::mat4 rotationMatrix = rotationZMatrix * rotationYMatrix * rotationXMatrix;
+		const glm::mat4 rotationXMatrix = glm::rotate(glm::radians(rotation.x), 
+			glm::vec3(1.0, 0.0, 0.0));
+		const glm::mat4 rotationYMatrix = glm::rotate(glm::radians(rotation.y), 
+			glm::vec3(0.0, 1.0, 0.0));
+		const glm::mat4 rotationZMatrix = glm::rotate(glm::radians(rotation.z),
+			glm::vec3(0.0, 0.0, 1.0));
+
+		const glm::mat4 scaleMatrix = glm::scale(scale);
+
+		const glm::mat4 rotationMatrix = rotationZMatrix * rotationYMatrix * rotationXMatrix;
 
 		// Matrix multiplication is applied in reverse order
 		return positionMatrix * rotationMatrix * scaleMatrix;
 	}
 
-	inline glm::vec3& GetPosition() { return position; }
-	inline glm::vec3& GetRotation() { return rotation; }
-	inline glm::vec3& GetScale() { return scale; }
+	[[nodiscard]] glm::vec3& GetPosition() { return position; }
+	[[nodiscard]] glm::vec3& GetRotation() { return rotation; }
+	[[nodiscard]] glm::vec3& GetScale() { return scale; }
 
-	inline void SetPosition(const glm::vec3& position) { this->position = position; }
-	inline void SetRotation(const glm::vec3& rotation) { this->rotation = rotation; }
-	inline void SetScale(const glm::vec3& scale) { this->scale = scale; }
+	[[nodiscard]] const glm::vec3& GetPosition() const { return position; }
+	[[nodiscard]] const glm::vec3& GetRotation() const { return rotation; }
+	[[nodiscard]] const glm::vec3& GetScale() const { return scale; }
 
-protected:
+	void SetPosition(const glm::vec3& position) { this->position = position; }
+	void SetRotation(const glm::vec3& rotation) { this->rotation = rotation; }
+	void SetScale(const glm::vec3& scale) { this->scale = scale; }
+
 private:
 	glm::vec3 position;
 	glm::vec3 rotation;
